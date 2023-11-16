864e2d95a22723681cc7e49788bf20e5ddfdc053
David Alonso Trujillo <148484178+DAT-995@users.noreply.github.com>
2023-11-16 19:24:45 +0000

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,45 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    Pescado.setPosition(randint(0, 160), randint(0, 120))
+    info.startCountdown(20)
+    music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.UntilDone)
+})
+let Pescado: Sprite = null
+scene.setBackgroundColor(8)
+let Tiburon = sprites.create(img`
+    ...........fffffff...ccfff..........
+    ..........fbbbbbbbffcbbbbf..........
+    ..........fbb111bbbbbffbf...........
+    ..........fb11111ffbbbbff...........
+    ..........f1cccc1ffbbbbbcff.........
+    ..........ffc1c1c1bbcbcbcccf........
+    ...........fcc3331bbbcbcbcccf..ccccc
+    ............c333c1bbbcbcbccccfcddbbc
+    ............c333c1bbbbbbbcccccddbcc.
+    ............c333c11bbbbbccccccbbcc..
+    ...........cc331c11bbbbccccccfbccf..
+    ...........cc13c11cbbbcccccbbcfccf..
+    ...........c111111cbbbfdddddc.fbbcf.
+    ............cc1111fbdbbfdddc...fbbf.
+    ..............cccfffbdbbfcc.....fbbf
+    ....................fffff........fff
+    `, SpriteKind.Player)
+controller.moveSprite(Tiburon)
+Pescado = sprites.create(img`
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 b c 5 5 d 4 c . . 
+    . b b b b 5 5 5 b f d d 4 4 4 b 
+    . b d 5 b 5 5 b c b 4 4 4 4 b . 
+    . . b 5 5 b 5 5 5 4 4 4 4 b . . 
+    . . b d 5 5 b 5 5 5 5 5 5 b . . 
+    . b d b 5 5 5 d 5 5 5 5 5 5 b . 
+    b d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c d d d c c b 5 5 5 5 5 5 5 b . 
+    c b d d d d d 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Food)
